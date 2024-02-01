25fc4621a87b077cee7c0c95d42b47f6600c5c0e
DESKTOP-DN9VFS9\3DDL <alperasar1@gmail.com>
2024-02-01 11:24:02 +0900

    opencv include the project

--- Qt_OpenCV.cpp
+++ Qt_OpenCV.cpp
@@ -1,9 +1,11 @@
 #include "Qt_OpenCV.h"
+#include <QtWidgets/QApplication>
 
 Qt_OpenCV::Qt_OpenCV(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
 }
 
 Qt_OpenCV::~Qt_OpenCV()
